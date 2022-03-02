88f2906f76d0e4a9abdb80ca16c241b502daf35a
fleroviux <fleroviux@gmail.com>
2022-03-02 20:20:01 +0100

    Update timer reload test

--- timer/reload/source/main.c
+++ timer/reload/source/main.c
@@ -37,7 +37,7 @@ IWRAM_CODE __attribute__((naked)) u16 test_reload(u16 reload) {
   );
 }
 
-IWRAM_CODE __attribute__((naked)) u16 test_reload_overwrite_16(u16 reload) {
+IWRAM_CODE __attribute__((naked)) u16 test_reload_overwrite_16_0(u16 reload) {
   asm(
     "ldr r1, =#0x04000100\n"
     "ldr r2, =#0xDEAD\n"
@@ -67,7 +67,7 @@ IWRAM_CODE __attribute__((naked)) u16 test_reload_overwrite_16(u16 reload) {
   );
 }
 
-IWRAM_CODE __attribute__((naked)) u16 test_reload_overwrite_32(u16 reload) {
+IWRAM_CODE __attribute__((naked)) u16 test_reload_overwrite_32_0(u16 reload) {
   asm(
     "ldr r1, =#0x04000100\n"
     "ldr r2, =#0x0080DEAD\n"
@@ -97,7 +97,37 @@ IWRAM_CODE __attribute__((naked)) u16 test_reload_overwrite_32(u16 reload) {
   );
 }
 
-IWRAM_CODE __attribute__((naked)) u16 test_reload_overwrite_32_late(u16 reload) {
+IWRAM_CODE __attribute__((naked)) u16 test_reload_overwrite_32_6(u16 reload) {
+  asm(
+    "ldr r1, =#0x04000100\n"
+    "ldr r2, =#0x0080DEAD\n"
+
+    // reset TM0
+    "mov r3, #0\n"
+    "str r3, [r1]\n"
+
+    // start TM0 with given reload
+    "mov r3, #0x00800000\n"
+    "orr r3, r0\n"
+    "str r3, [r1]\n"
+
+    // delay, then overwrite RELOAD in the next-to-last cycle
+    "nop\n"
+    "nop\n"
+    "nop\n"
+    "nop\n"
+    "nop\n"
+    "nop\n"
+    "str r2, [r1]\n"
+    "nop\n"
+
+    // read result and return
+    "ldrh r0, [r1]\n"
+    "bx lr\n"
+  );
+}
+
+IWRAM_CODE __attribute__((naked)) u16 test_reload_overwrite_32_7(u16 reload) {
   asm(
     "ldr r1, =#0x04000100\n"
     "ldr r2, =#0x0080DEAD\n"
@@ -131,11 +161,12 @@ IWRAM_CODE int main(void) {
   consoleDemoInit();
 
   expect_hex("FFF8 -- -", 0xFFF9, test_reload(0xFFF8));
-  expect_hex("FFF8 16 0", 0xDEAE, test_reload_overwrite_16(0xFFF8));
-  expect_hex("FFF8 32 0", 0xDEAE, test_reload_overwrite_32(0xFFF8));
-  expect_hex("FFF8 32 7", 0xFFF9, test_reload_overwrite_32_late(0xFFF8));
-  expect_hex("FFFF 16 0", 0xDEB4, test_reload_overwrite_16(0xFFFF));
-  expect_hex("FFFF 32 0", 0xDEB4, test_reload_overwrite_32(0xFFFF));
+  expect_hex("FFF8 16 0", 0xDEAE, test_reload_overwrite_16_0(0xFFF8));
+  expect_hex("FFF8 32 0", 0xDEAE, test_reload_overwrite_32_0(0xFFF8));
+  expect_hex("FFF8 32 6", 0xDEAE, test_reload_overwrite_32_6(0xFFF8));
+  expect_hex("FFF8 32 7", 0xFFF9, test_reload_overwrite_32_7(0xFFF8));
+  expect_hex("FFFF 16 0", 0xDEB4, test_reload_overwrite_16_0(0xFFFF));
+  expect_hex("FFFF 32 0", 0xDEB4, test_reload_overwrite_32_0(0xFFFF));
   
   print_metrics();
 
