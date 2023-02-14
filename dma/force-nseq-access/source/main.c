fb25f77ef55d711d730f2c2c04bbc95e5f999163
fleroviux <fleroviux@gmail.com>
2023-02-14 23:13:23 +0100

    Add a test for sequential ROM access after DMA

---
+++ dma/force-nseq-access/source/main.c
@@ -0,0 +1,77 @@
+
+#include <gba_console.h>
+#include <gba_dma.h>
+#include <gba_timers.h>
+#include <stdio.h>
+
+#include "test.h"
+
+/*int IWRAM_CODE test_immediate() {
+  u16 result = 0;
+
+  REG_TM0CNT_H = 0;
+  REG_TM0CNT_L = 0;
+  REG_TM0CNT_H = TIMER_START;
+
+  REG_DMA0SAD = (u32)&REG_TM0CNT_L;
+  REG_DMA0DAD = (u32)&result;
+  REG_DMA0CNT = DMA_ENABLE | DMA16 | DMA_IMMEDIATE | 1;
+  while (REG_DMA0CNT & DMA_ENABLE);
+
+  test_expect("IMM", 20, result);
+}*/
+
+void __attribute__((naked)) __attribute__ ((noinline)) __test(u32 address, u32 value) {
+  asm(
+    "str r1, [r0]\n"
+    "mov r0, r0\n"
+    "mov r0, r0\n"
+    "mov r0, r0\n"
+    "mov r0, r0\n"
+    "bx lr"
+  );
+}
+
+void test_ewram() {
+  REG_DMA0SAD = 0x02000000;
+  REG_DMA0DAD = 0x02000000;
+
+  REG_TM0CNT_H = 0;
+  REG_TM0CNT_L = 0;
+  REG_TM0CNT_H = TIMER_START;
+
+  __test((u32)&REG_DMA0CNT, DMA_ENABLE | DMA16 | DMA_IMMEDIATE | 1);
+
+  u16 result = REG_TM0CNT_L;
+
+  test_expect("EWRAM DMA", 88, result);
+}
+
+void test_rom() {
+  u16 tmp;
+  REG_DMA0SAD = 0x08000000;
+  REG_DMA0DAD = (u32)&tmp;
+
+  REG_TM0CNT_H = 0;
+  REG_TM0CNT_L = 0;
+  REG_TM0CNT_H = TIMER_START;
+
+  __test((u32)&REG_DMA0CNT, DMA_ENABLE | DMA16 | DMA_IMMEDIATE | 1);
+
+  u16 result = REG_TM0CNT_L;
+
+  test_expect("ROM DMA", 84, result);
+}
+
+int main(void) {
+  consoleDemoInit();
+
+  test_ewram();
+  test_rom();
+  test_print_metrics();
+
+  while (1) {
+  }
+}
+
+
